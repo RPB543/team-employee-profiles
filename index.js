a20e8622e3148c0484100dd71e4a4425f6bf204e
Becca Byrne <aggiebec06@gmail.com>
2021-12-08 11:58:58 -0600

    added file structure

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// link team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
