dc880e89835da631fb0235595e484f9aef9bfc67
Becca Byrne <aggiebec06@gmail.com>
2021-12-08 12:46:48 -0600

    added employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,24 @@ test('creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('123go@gmail.com');
 });
+
+// gets id from getId() 
+test('gets employee ID', () => {
+    const employee = new Employee('Becca', 21, '123go@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// gets emails from getEmail()
+test('gets employee email', () => {
+    const employee = new Employee('123go@gmail.com');
+
+    expect(employee.getEmail()).toBe(employee.email);
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const employee = new Employee('Becca');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,10 @@
+const Engineer = require('../lib/Engineer.js');
+
+// // creates employee object 
+// test('receives engineer github object', () => {
+//     const engineer = new Engineer('Becca', 21, '123go@gmail.com');
+
+//     expect(employee.name).toBe('Becca');
+//     expect(employee.id).toEqual(expect.any(Number));
+//     expect(employee.email).toBe('123go@gmail.com');
+// });
